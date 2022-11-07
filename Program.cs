b571c08594608a1c363a09ad12856647b7c7e26e
k-okawa <kyohei.okawa@10antz.co.jp>
2022-11-07 11:26:19 +0900

    reflection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Reflection;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
+    internal class Sample {
+        public int test = 1;
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var sample = new Sample();
+            FieldInfo[] fields = sample.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
+            foreach (var field in fields) {
+                Console.WriteLine(field.Name);
+            }
         }
     }
 }
